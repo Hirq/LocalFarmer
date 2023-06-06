623f0efad77ce13d71ce1806dd9140611b6760d1
Zabrocki-PC <czarkooo@gmail.com>
2023-06-06 21:52:41 +0200

    [Web] Alert

--- LocalFarmer.Web/Services/AlertService.cs
+++ LocalFarmer.Web/Services/AlertService.cs
@@ -3,5 +3,12 @@
     public class AlertService
     {
         public bool IsSuccessAlert { get; set; }
+        public string SuccessText { get; set; }
+
+        public void SetSuccessAlert(string text)
+        {
+            IsSuccessAlert = true;
+            SuccessText = text;
+        }
     }
 }
