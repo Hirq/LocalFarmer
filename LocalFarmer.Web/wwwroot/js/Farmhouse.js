ff0a1c28ef9239230069202b9f1aa6c0111d8e7b
Zabrocki-PC <czarkooo@gmail.com>
2023-06-20 23:14:02 +0200

    [Web] Add map

--- LocalFarmer.Web/wwwroot/js/Farmhouse.js
+++ LocalFarmer.Web/wwwroot/js/Farmhouse.js
@@ -1,8 +1,14 @@
-﻿function closeAlertAfterDelay() {
+﻿var map;
+
+function closeAlertAfterDelay() {
     setTimeout(function () {
         var alertElement = document.querySelector('.mud-alert');
         if (alertElement) {
             alertElement.style.display = 'none';
         }
     }, 2500);
+}
+
+function initializeGoogleMap() {
+    map = L.map('map').setView([51.505, -0.09], 13);
 }
