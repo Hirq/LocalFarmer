cdda6d94c79eb9a118341c37a7f6068ad50360d0
Zabrocki-PC <czarkooo@gmail.com>
2023-06-21 23:15:21 +0200

    [Web] Leaflet work - first map view

--- LocalFarmer.Web/wwwroot/js/Farmhouse.js
+++ LocalFarmer.Web/wwwroot/js/Farmhouse.js
@@ -1,14 +1,8 @@
-﻿var map;
-
-function closeAlertAfterDelay() {
+﻿function closeAlertAfterDelay() {
     setTimeout(function () {
         var alertElement = document.querySelector('.mud-alert');
         if (alertElement) {
             alertElement.style.display = 'none';
         }
     }, 2500);
-}
-
-function initializeGoogleMap() {
-    map = L.map('map').setView([51.505, -0.09], 13);
 }
