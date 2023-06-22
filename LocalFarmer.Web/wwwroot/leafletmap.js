47840844f809bc2b03e32a20128c8374b84e98c6
Zabrocki-PC <czarkooo@gmail.com>
2023-06-22 16:28:05 +0200

    set map on Koszalin City

--- LocalFarmer.Web/wwwroot/leafletmap.js
+++ LocalFarmer.Web/wwwroot/leafletmap.js
@@ -1,4 +1,5 @@
 ﻿export function load_map() {
-    let map = L.map('map').setView({ lon: 0, lat: 0 }, 2);
+    //let map = L.map('map').setView({ lon: 26, lat: 44 }, 10);
+    let map = L.map('map').setView([54.22, 16.11], 12);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 19 }).addTo(map);
 }
