2d129b4724d0b59b970f9adc3b1f3a108fd45ec1
Zabrocki-PC <czarkooo@gmail.com>
2023-07-02 20:42:54 +0200

    Add Roles

--- LocalFarmer.Data/Context/LocalfarmerDbContext.cs
+++ LocalFarmer.Data/Context/LocalfarmerDbContext.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Net.NetworkInformation;
 
 namespace LocalFarmer.Data.Context
 {
@@ -11,6 +12,31 @@ namespace LocalFarmer.Data.Context
             : base(options)
         {
 
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            SeedRoles(builder);
+        }
+
+        private static void SeedRoles(ModelBuilder builder)
+        {
+            builder.Entity<IdentityRole>().HasData
+                (
+                    new IdentityRole()
+                    {
+                        Name = "Admin",
+                        ConcurrencyStamp = "1",
+                        NormalizedName = "Admin"
+                    },
+                    new IdentityRole()
+                    {
+                        Name = "User",
+                        ConcurrencyStamp = "2",
+                        NormalizedName = "User"
+                    }
+                );
         }
 
         public DbSet<Farmhouse> Farmhouses { get; set; }
