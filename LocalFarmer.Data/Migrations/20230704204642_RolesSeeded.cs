ba361bc0b93a8d94275c5ffd4ea6f272efc9621a
Zabrocki-PC <czarkooo@gmail.com>
2023-07-04 22:47:35 +0200

    Seed roles

---
+++ LocalFarmer.Data/Migrations/20230704204642_RolesSeeded.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LocalFarmer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class RolesSeeded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "40cd909d-adb3-43ad-9876-54a87078abf8", "1", "Admin", "Admin" },
+                    { "fd83a152-4415-4999-bbd9-2d95d06d3482", "2", "User", "User" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "40cd909d-adb3-43ad-9876-54a87078abf8");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "fd83a152-4415-4999-bbd9-2d95d06d3482");
+        }
+    }
+}
