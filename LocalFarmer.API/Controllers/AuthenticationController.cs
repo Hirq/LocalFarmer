2468aa58e3ee659d5204c5c01058da718dc73b25
Zabrocki-PC <czarkooo@gmail.com>
2023-07-06 22:59:49 +0200

    add user

--- LocalFarmer.API/Controllers/AuthenticationController.cs
+++ LocalFarmer.API/Controllers/AuthenticationController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using LocalFarmer.API.Enum;
+using LocalFarmer.API.Utilities;
+using LocalFarmer.Domain.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LocalFarmer.API.Controllers
@@ -21,7 +24,36 @@ namespace LocalFarmer.API.Controllers
         [HttpPost]
         public async Task<IActionResult> Register([FromBody] RegisterUserDto registerUser, string role)
         {
+            var userExist = await _userManager.FindByEmailAsync(registerUser.Email);
+            if (userExist == null)
+            {
+                return StatusCode(StatusCodes.Status403Forbidden, new Response
+                {
+                    Status = StatusResponse.Error,
+                    Message = "User already exixst!"
+                });
+            }
 
+            var user = new ApplicationUser()
+            {
+                Email = registerUser.Email,
+                UserName = registerUser.Username,
+                SecurityStamp = Guid.NewGuid().ToString(),
+            };
+
+            var result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            return result.Succeeded
+                ? StatusCode(StatusCodes.Status201Created, new Response
+                {
+                    Status = StatusResponse.Success,
+                    Message = "User created successfully!"
+                })
+                : (IActionResult)StatusCode(StatusCodes.Status500InternalServerError, new Response
+                {
+                    Status = StatusResponse.Error,
+                    Message = "User failed to create!"
+                });
         }
     }
 }
---
+++ LocalFarmer.API/Enum/StatusResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace LocalFarmer.API.Enum
+{
+    public class StatusResponse
+    {
+        public const string Success = "Success";
+        public const string Error = "Error";
+    }
+}
