ca3f4a9a221f431765a5037c7deacb069790be26
Zabrocki-PC <czarkooo@gmail.com>
2023-07-14 18:44:47 +0200

    Prepare service register user

---
+++ LocalFarmer.Web/Services/Interfaces/IUserService.cs
@@ -0,0 +1,9 @@
+﻿using static LocalFarmer.Web.Pages.Account.Register;
+
+namespace LocalFarmer.Web.Services
+{
+    public interface IUserService
+    {
+        public Task AddUser(RegisterAccountForm registerUser, string role);
+    }
+}
---
+++ LocalFarmer.Web/Services/UserService.cs
@@ -0,0 +1,27 @@
+﻿using LocalFarmer.Domain.Models;
+using static LocalFarmer.Web.Pages.Account.Register;
+
+namespace LocalFarmer.Web.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly HttpClient _http;
+
+        public UserService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task AddUser(RegisterAccountForm registerUser, string role)
+        {
+            RegisterUserDto userDto = new RegisterUserDto()
+            {
+                Username = registerUser.Email,
+                Email= registerUser.Email,
+                Password = registerUser.Password,
+            };
+
+            await _http.PostAsJsonAsync($"https://localhost:7290/api/Authentication?role={role}", userDto);
+        }
+    }
+}
